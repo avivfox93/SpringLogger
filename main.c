07ad8a76af164c90ab24399ad34e88ead0c5ecb1
Aviv Fox <avivfox93@gmail.com>
2021-12-18 08:50:05 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 int main() {
     struct SpringLogger_Settings_t settings;
-    settings.level = SPRING_LOGGER_LOG_LEVEL_DEBUG;
+    settings.level = SPRING_LOGGER_LOG_LEVEL_VERBOSE;
     settings.time = NULL; // use default time printing (epoch using time(NULL) function)
     settings.colors = 1;
     settings.flush = 1;
-    SpringLogger_Init(stdout,NULL);
+    SpringLogger_Init(stdout,&settings);
     const char* TAG = "Spring";
     int i = 0;
     SpringLogger_LOG_Location(SPRING_LOGGER_LOG_LEVEL_ERROR,TAG,"Unknown ERROR!");
